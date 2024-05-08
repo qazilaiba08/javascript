687e04e5d2f1071fa725b9b0f67ed313f622b712
qazilaiba08 <164245574+qazilaiba08@users.noreply.github.com>
2024-05-08 16:05:04 +0500

    java

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+let num = 13;
+let newNum = ++num ;//12+1 = 13
+console.log(num);
+console.log(newNum);
+
+// const totalCost = 1 + 3 * 4;
+// console.log(totalCost);
+
+let cost = (1 + 3) * 4;
+console.log(cost);
+
+var spec = prompt ("Your species?", "human jhkdfsdeo-");
